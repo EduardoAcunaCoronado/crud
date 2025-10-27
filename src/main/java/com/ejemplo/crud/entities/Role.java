90e943a3f3dce69b3a94d654c5505ab4fae8a6f1
eduardoacunacoronado <edu191191@gmail.com>
2025-10-27 14:40:39 +0100

    ManyToMany Bidireccinal revisando error Bucle.

--- src/main/java/com/ejemplo/crud/entities/Role.java
+++ src/main/java/com/ejemplo/crud/entities/Role.java
@@ -1,6 +1,10 @@
 package com.ejemplo.crud.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "roles")
@@ -13,8 +17,12 @@ public class Role {
     @Column(unique = true)
     private String name;
 
-    public Role() {
+    @JsonIgnoreProperties({"roles", "hibernateLazyInitializer", "handler"})
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
 
+    public Role() {
+        this.users = new ArrayList<>();
     }
 
     public Role(String name) {
@@ -33,5 +41,44 @@ public class Role {
     }
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Role other = (Role) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
     }
 }
--- src/main/java/com/ejemplo/crud/entities/User.java
+++ src/main/java/com/ejemplo/crud/entities/User.java
@@ -1,11 +1,13 @@
 package com.ejemplo.crud.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -32,6 +34,7 @@ public class User {
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private boolean admin;
 
+    @JsonIgnoreProperties({"users", "hibernateLazyInitializer", "handler"})
     @ManyToMany
     @JoinTable(
             name = "users_roles",
@@ -41,7 +44,7 @@ public class User {
     private List<Role> roles;
 
     public User() {
-
+        this.roles = new ArrayList<>();
     }
 
     public User(String username, String password) {
@@ -101,4 +104,37 @@ public class User {
     public void setAdmin(boolean admin) {
         this.admin = admin;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((username == null) ? 0 : username.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (username == null) {
+            if (other.username != null)
+                return false;
+        } else if (!username.equals(other.username))
+            return false;
+        return true;
+    }
+
+
 }
